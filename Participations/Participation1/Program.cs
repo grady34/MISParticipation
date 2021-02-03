8de51f956666dd93063068426af7dfdb1ba2db9f
Grady Allen <grady.t.allen-1@ou.edu>
2021-02-03 13:42:34 -0600

    SumofNumbers Participation.

--- Participations/Participation1/Program.cs
+++ Participations/Participation1/Program.cs
@@ -6,7 +6,30 @@ namespace Participation1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a Number");
+            string number1 = Console.ReadLine();
+            Console.WriteLine("Enter another number");
+            string number2 = Console.ReadLine();
+            Console.WriteLine("Enter one more number");
+            string number3 = Console.ReadLine();
+
+            double result1;
+            double result2;
+            double result3;
+
+            result1 = Convert.ToDouble(number1);
+            result2 = Convert.ToDouble(number2);
+            result3 = Convert.ToDouble(number3);
+
+            double output1 = result1 + result2 + result3;
+            Console.WriteLine("The sum of the numbers you entered is " + Math.Round(output1, 3));
+
+            const double MAGIC_NUMBER = 7.777; // local variable that is a constant
+
+            double output2 = Math.Round(output1, 3) * MAGIC_NUMBER;
+            Console.WriteLine("The product of 7.777 and the sum of the three numbers is " + Math.Round(output2, 3));
+
+
         }
     }
 }
