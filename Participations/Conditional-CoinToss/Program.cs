358b92cd46a0332f89bbc86cffbb586171343741
Grady Allen <Grady.T.Allen-1@ou.edu>
2021-02-08 14:19:52 -0600

    Finished Coin Toss

--- Participations/Conditional-CoinToss/Program.cs
+++ Participations/Conditional-CoinToss/Program.cs
@@ -6,16 +6,54 @@ namespace Conditional_CoinToss
     {
         static void Main(string[] args)
         {
+            const string devname = "Grady Allen";
+
             Console.WriteLine("Please pick either heads or tails. >>");
             string answer = Console.ReadLine();
-            
-            if (answer == "heads")
-            {
 
+            if (answer.ToLower() == "heads")
+            {
+              
             }
+            else if (answer.ToLower() == "tails")
+            {
+               
+            }
+            else
+            {
+                Console.WriteLine($"ERROR. {answer} is not heads or tails.");
+                Console.WriteLine($"This aplication was created by {devname}.");
+                return;
+            }
+
             Random rand = new Random();
             int randomNbr = rand.Next(1, 3);
-            Console.WriteLine($"{randomNbr}");
+
+            if (randomNbr == 1)
+            {
+                if (answer.ToLower() == "heads")
+                {
+                    Console.WriteLine("You were CORRECT the coin landed on HEADS");
+                }
+                else if (answer.ToLower() == "tails")
+                {
+                    Console.WriteLine("You were INCORRECT the coin landed on HEADS");
+                }
+            }
+            else if (randomNbr == 2)
+            {
+                if (answer.ToLower() == "tails")
+                {
+                    Console.WriteLine("You were CORRECT the coin landed on TAILS");
+                }
+                else if (answer.ToLower() == "heads")
+                {
+                    Console.WriteLine("You were INCORRECT the coin landed on TAILS");
+                }
+            }
+
+            Console.WriteLine($"This aplication was created by {devname}.");
+            return;
         }
     }
 }
