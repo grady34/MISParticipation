ba29cac3bdf6981fa2d40b427a00ad5c79934183
Grady Allen <grady.t.allen-1@ou.edu>
2021-02-22 14:02:59 -0600

    started Random Guess

--- RandomGuess/RandomGuess/Program.cs
+++ RandomGuess/RandomGuess/Program.cs
@@ -6,7 +6,29 @@ namespace RandomGuess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Welcome to the Random Number Guessing Game!!");
+
+            string lowlimit;
+            string uplimit;
+
+            Console.WriteLine("What would you like the lower limit to be? >>");
+            lowlimit = Console.ReadLine();
+            int lowint = Convert.ToInt32(lowlimit);
+
+            Console.WriteLine("What would you like the upper limit to be? >>");
+            uplimit = Console.ReadLine();
+            int upint = Convert.ToInt32(uplimit);
+
+            Random rand = new Random();
+            int randomNbr = rand.Next(lowint, upint + 1);
+
+            Console.WriteLine("Guess a number between your lower and upper limit. >>");
+            // need to make sure they only enter a number tryparse
+            // need to make a do while loop so if they guess wrong they can try again and again until they guess correct
+           
+
+
         }
     }
 }
