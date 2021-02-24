4d2a0b273530b026121cd2dbec08527ef3c354b3
Grady Allen <grady.t.allen-1@ou.edu>
2021-02-24 12:49:36 -0600

    added try parse

--- RandomGuess/RandomGuess/Program.cs
+++ RandomGuess/RandomGuess/Program.cs
@@ -11,6 +11,7 @@ namespace RandomGuess
 
             string lowlimit;
             string uplimit;
+            string guess;
 
             Console.WriteLine("What would you like the lower limit to be? >>");
             lowlimit = Console.ReadLine();
@@ -24,9 +25,21 @@ namespace RandomGuess
             int randomNbr = rand.Next(lowint, upint + 1);
 
             Console.WriteLine("Guess a number between your lower and upper limit. >>");
+            guess = Console.ReadLine();
+
+            int numguess;
+
+            while (int.TryParse(guess, out numguess) == false)
+            {
+                Console.WriteLine("Incorrect value.  Your guess must be an integer >>");
+                guess = Console.ReadLine();
+            }
+
+
+
             // need to make sure they only enter a number tryparse
             // need to make a do while loop so if they guess wrong they can try again and again until they guess correct
-           
+
 
 
         }
