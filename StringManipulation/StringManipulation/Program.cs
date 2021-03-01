9d2bbb2de3d13601ff054257d10ce75250a46334
Grady Allen <Grady.T.Allen-1@ou.edu>
2021-03-01 13:34:45 -0600

    .

--- StringManipulation/StringManipulation/Program.cs
+++ StringManipulation/StringManipulation/Program.cs
@@ -6,7 +6,30 @@ namespace StringManipulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string statement = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+
+            Console.WriteLine(statement);
+
+            Console.WriteLine("Read the above statement and type in a word you want to look for in the above sentence. >>");
+            string word = Console.ReadLine();
+            Console.WriteLine("What word would you like to use as a replacement? >>");
+            string newword = Console.ReadLine();
+
+            bool contains = statement.Contains(word);
+
+            if (contains)
+            {
+                Console.WriteLine(statement.Replace(word, newword));
+            }
+            else
+            {
+                Console.WriteLine("Sorry, I could not find your word " + word + ".");
+            }
+            for (int i = word.Length; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
