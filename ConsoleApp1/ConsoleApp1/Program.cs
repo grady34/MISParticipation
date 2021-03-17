dfd2e08b4f403fc2d249e466c7f2fa5c875fc54d
Grady Allen <grady.t.allen-1@ou.edu>
2021-03-17 15:01:55 -0500

    ..

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,18 +10,19 @@ namespace ConsoleApp1
             Console.WriteLine("How many exam scores do you have?");
             int numofscores = Convert.ToInt32(Console.ReadLine());
             List<double> scores = new List<double>();
-            Dictionary<int, double> dictscores = new Dictionary<int, double>();
+            Dictionary<int, int> dictscores = new Dictionary<int, int>();
+            //use a dictionary to setup a way to count the mode. then relate the id's back to the original numbers. The id's should have the same value as the indexes. 
             double average = 0;
             double min = 0;
             double max = 0;
             double mode = 0;
+            int modenum = 0;
 
             for (int i = 0; i < numofscores; i++)
             {
                 Console.WriteLine("Enter an exam score.");
                 double examscores = Convert.ToDouble(Console.ReadLine());
                 scores.Add(examscores);
-                dictscores.Add(i, examscores);
             }
 
             for (int i = 1; i < numofscores; i++)
@@ -36,15 +37,12 @@ namespace ConsoleApp1
                 {
                     max = scores[i];
                 }
-                
-                
-                
-
             }
             average = average / numofscores;
             Console.WriteLine($"The average of your exam scores is {Math.Round(average, 2)}.");
             Console.WriteLine($"The minimum of your exam scores is {min}.");
             Console.WriteLine($"The maximum of your exam scores is {max}.");
+            Console.WriteLine($"The mode of your exam scores is {mode}.");
         }
     }
 }
