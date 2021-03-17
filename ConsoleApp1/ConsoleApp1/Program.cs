fd00d6490a7a02432395090bd545d084acdc962f
Grady Allen <Grady.T.Allen-1@ou.edu>
2021-03-17 17:48:06 -0500

    finished

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,19 +10,19 @@ namespace ConsoleApp1
             Console.WriteLine("How many exam scores do you have?");
             int numofscores = Convert.ToInt32(Console.ReadLine());
             List<double> scores = new List<double>();
-            Dictionary<int, int> dictscores = new Dictionary<int, int>();
-            //use a dictionary to setup a way to count the mode. then relate the id's back to the original numbers. The id's should have the same value as the indexes. 
+            Dictionary<double, double> dictscores = new Dictionary<double, double>();
             double average = 0;
             double min = 0;
             double max = 0;
             double mode = 0;
-            int modenum = 0;
+            double modenum = 0;
 
             for (int i = 0; i < numofscores; i++)
             {
                 Console.WriteLine("Enter an exam score.");
                 double examscores = Convert.ToDouble(Console.ReadLine());
                 scores.Add(examscores);
+                
             }
 
             for (int i = 1; i < numofscores; i++)
@@ -37,7 +37,28 @@ namespace ConsoleApp1
                 {
                     max = scores[i];
                 }
+                
             }
+            foreach (var item in scores)
+            {
+                if (dictscores.ContainsKey(item))
+                {
+                    dictscores[item]++;
+                }
+                else
+                {
+                    dictscores.Add(item, 1);
+                }
+            }
+            foreach (KeyValuePair<double, double> number in dictscores)
+            {
+                if (number.Value > modenum )
+                {
+                    mode = number.Key;
+                    modenum = number.Value;
+                }
+            }
+            
             average = average / numofscores;
             Console.WriteLine($"The average of your exam scores is {Math.Round(average, 2)}.");
             Console.WriteLine($"The minimum of your exam scores is {min}.");
