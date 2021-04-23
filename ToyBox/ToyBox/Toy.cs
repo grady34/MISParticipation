b376c88a433b7bf6927f5a9e0f6934a52bc373e2
Grady Allen <grady.t.allen-1@ou.edu>
2021-04-23 18:13:15 -0500

    ..

---
+++ ToyBox/ToyBox/Toy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyBox
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+
+        public double Price { get; set; }
+
+
+        private string Notes;
+        public string GetNotes()
+        {
+            return Notes;
+        }
+
+        public void SetNotes(string note)
+        {
+            Notes = note;
+        }
+
+
+        public Toy()
+        {
+            Manufacturer = "N/A";
+            Name = "N/A";
+            Price = 0.00;
+            Notes = "N/A";
+        }
+
+        public Toy(string manufacturer, string name, double price, string notes)
+        {
+            this.Manufacturer = manufacturer;
+            this.Name = name;
+            this.Price = price;
+            this.Notes = notes;
+        }
+
+        public string GetAisle()
+        {
+            string letter = Manufacturer.Substring(startIndex: 0, 1);
+            Random rand = new Random();
+            int aislenum = rand.Next(1, 25);
+            string aisle = letter.ToUpper() + Convert.ToString(aislenum);
+
+            return aisle;
+        }
+    }
+}
---
+++ ToyBox/ToyBox/ToyBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyBox
+{
+    class ToyBox
+    {
+        public List<Toy> Toys { get; set; }
+
+        public string Owner { get; set; }
+
+        public string Location { get; set; }
+
+        public ToyBox()
+        {
+
+        }
+
+        public Toy GetRandomToy()
+        {
+            int number = Toys.Count;
+            Random rand = new Random();
+            int randnumber = rand.Next(0, number);
+            Toy randtoy = Toys[randnumber];
+            return randtoy;
+        }
+    }
+}
