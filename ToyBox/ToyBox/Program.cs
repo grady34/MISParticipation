a8ecfa11d3c661d7f69f517dc4a457c9f9731d1f
Grady Allen <Grady.T.Allen-1@ou.edu>
2021-04-23 19:33:46 -0500

    ..

--- ToyBox/ToyBox/Program.cs
+++ ToyBox/ToyBox/Program.cs
@@ -7,6 +7,8 @@ namespace ToyBox
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the name of the toybox");
+
+            //didn't finish. I didn't fully understand the required tasks for the participation. I'm looking forward to see how you do it in class. 
         }
     }
 }
